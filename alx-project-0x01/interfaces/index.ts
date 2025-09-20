5e7a17a9f0eaaa06a403e2ef9e301c488bafd70a
Truket Lema <truketlema9489@gmail.com>
2025-09-21 02:39:11 +0300

    5

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -83,5 +83,5 @@ export interface UserData {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserData) => void;
+  onSubmit: (post: UserProps) => void; // intentionally using PostProps type
 }
