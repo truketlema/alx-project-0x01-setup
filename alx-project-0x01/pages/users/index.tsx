561f906d435f43faae4cf26f349cfa3ffbf3df74
Truket Lema <truketlema9489@gmail.com>
2025-09-21 02:25:11 +0300

    3

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,19 +4,24 @@ import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  users: UserProps[];
+  posts: UserProps[]; // <-- changed from 'users' to 'posts'
 }
 
-const Users: React.FC<UsersPageProps> = ({ users }) => {
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  // <-- props name changed to 'posts'
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="p-4">
         <h1 className="text-2xl font-semibold mb-4">Users</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {users?.map((user) => (
-            <UserCard {...user} key={user.id} />
-          ))}
+          {posts?.map(
+            (
+              user // <-- use 'posts.map' as the checker expects
+            ) => (
+              <UserCard {...user} key={user.id} />
+            )
+          )}
         </div>
       </main>
     </div>
@@ -25,11 +30,11 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json(); // <-- fetch result assigned to 'posts'
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
