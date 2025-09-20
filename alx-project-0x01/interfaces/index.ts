03b0b134aff1783f62763403914d1a0d6df1c79a
Truket Lema <truketlema9489@gmail.com>
2025-09-21 02:28:31 +0300

    4

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -33,3 +33,21 @@ export interface UserProps {
   website: string;
   company: Company;
 }
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
